ab3373461be4a46bf4c82aed46af341cd437e4a5
s-elo <zz321yy@qq.com>
2022-02-10 13:29:10 +0800

    improve the shortcut

--- run.bat
+++ run.bat
@@ -4,7 +4,5 @@ title doc-server.bat
 %switch to the script path%
 cd /d %~dp0
 
-start /min cmd /c "cd client && npm run start"
-
 %run the script%
 tsc && cd dist && node server.js
--- server/server.ts
+++ server/server.ts
@@ -2,22 +2,40 @@ import express from "express";
 import fs from "fs-extra";
 import path from "path";
 import showdown from "showdown";
+import open from "open";
 
 const server = express();
 
 const port = 5600;
 
+server.use("/", express.static(path.resolve(__dirname, "..", "client/build")));
+
 // Cross-Origin Resource Sharing
 server.all("*", (_, res, next) => {
-  res.header("Access-Control-Allow-Origin", `*`);
+  res.header("Access-Control-Allow-Origin", `localhost`);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
 
   next();
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+const app = server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  open(`http://localhost:${port}`);
+});
+
+app.on("error", () => {
+  // already listened
+  open(`http://localhost:${port}`);
+});
 
 server.get("/", (_, res) => {
+  const frontPage = fs.readFileSync(
+    path.resolve(__dirname, "..", "client/build/index.html"),
+    "utf-8"
+  );
+  return res.send(frontPage);
+});
+server.get("/get-article", (_, res) => {
   const file = fs.readFileSync(
     path.resolve(__dirname, "..", "README.md"),
     "utf-8"
