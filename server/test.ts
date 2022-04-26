34d6cb6fbbe1c12794164733aee4eb3f16d4edaa
s-elo <zz321yy@qq.com>
2022-04-26 10:21:22 +0800

    add simple git for git operation at nodejs and done some test on it

--- server/test.ts
+++ server/test.ts
@@ -1,6 +1,38 @@
 import { additDoc, modifyName } from "./docsOperaiton";
 import path from "path";
-
-const newContent = `# Header`;
+import simpleGit, { SimpleGit } from "simple-git";
+import { docRootPath } from "./getDocs";
+// const newContent = `# Header`;
 // additDoc(path.resolve(__dirname, "./test/article/a3.md"), newContent);
-modifyName("sort/sort", "newTree", true);
+// modifyName("sort/sort", "newTree", true);
+
+const getGitStatus = async (git: SimpleGit) => {
+  try {
+    const { not_added, staged } = await git.status();
+
+    if (not_added.length === 0 && staged.length === 0) return "COMMITTED";
+    else if (not_added.length === 0) return "ADDED";
+  } catch {
+    return "NOTGIT";
+  }
+};
+
+const gitTest = async (path: string) => {
+  const git = simpleGit(path);
+  console.log(await getGitStatus(git));
+
+  //   const pullRet = await git.pull();
+  //   console.log(pullRet);
+  const { tracking } = await git.status();
+  git.add("./*");
+  git.commit("just for test");
+  git.push(tracking?.split("/"));
+  //   if ((await git.pull())?.summary.changes) {
+  //     console.log()
+  //   }
+  //   const commands = ["status"];)
+  //   const ret = await git.raw(commands);
+  //   console.log(ret);
+};
+
+gitTest(docRootPath);
