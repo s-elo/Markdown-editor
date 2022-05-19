f027c99d9bd52521a6c18ab2bb2280ac844eafcc
s-elo <zz321yy@qq.com>
2022-05-19 10:40:35 +0800

    router switching when modifying name

--- client/src/components/OperationMenu/ModifyName.tsx
+++ client/src/components/OperationMenu/ModifyName.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectGlobalOpts } from "@/redux-feature/globalOptsSlice";
 import { useModifyDocNameMutation } from "@/redux-api/docsApi";
 import Toast from "@/utils/Toast";
+import { getCurrentPath } from "@/utils/utils";
 
 import { DOC } from "@/redux-api/docsApiType";
 
@@ -14,27 +16,26 @@ type ModifyNameProps = {
 
 export default function ModifyName({
   isFile, // a file or folder
-  path, // path that is clicked
+  path, // path that is clicked with original name
   siblings, // for repeated name checking
 }: ModifyNameProps) {
   const { themes, curTheme } = useSelector(selectGlobalOpts);
   const { backgroundColor } = themes[curTheme];
+
+  const routerHistory = useHistory();
+  const { pathname } = useLocation();
 
   // initialized as the original name
   const [newName, setNewName] = useState(path.slice(-1)[0]);
 
   const [modifyName] = useModifyDocNameMutation();
 
-  const modifyConfirm = async () => {
+  const modifyConfirm = useCallback(async () => {
     // original path that is being modified
     const modifyPath = path.join("-");
+    const newPath = path.slice(0, -1).concat(newName).join("-");
 
-    if (
-      siblings.some(
-        (doc) =>
-          doc.path.join("-") === path.slice(0, -1).concat(newName).join("-")
-      )
-    )
+    if (siblings.some((doc) => doc.path.join("-") === newPath))
       return Toast("the name is repeated!", "WARNING");
 
     try {
@@ -47,11 +48,17 @@ export default function ModifyName({
       // hidden
       document.body.click();
 
+      const curPath = getCurrentPath(pathname).join("-");
+      // modified path is the current path
+      if (curPath === modifyPath) {
+        routerHistory.push(`/article/${newPath}`);
+      }
+
       Toast("modified successfully!", "SUCCESS");
     } catch {
       Toast("failed to modify...", "ERROR");
     }
-  };
+  }, [path, isFile, siblings, newName, pathname, routerHistory, modifyName]);
 
   return (
     <div className="new-file-group-title">
