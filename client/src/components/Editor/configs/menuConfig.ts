dbb4f16579c2ed7cd3ae7ef1bc4eb3e0d9c13fc5
s-elo <zz321yy@qq.com>
2022-06-09 11:38:55 +0800

    Add more operations at editor menu
    add code fence, image, table and quote at the menu
    to address the issue where the above operations can not be done when
    indenting

--- client/src/components/Editor/configs/menuConfig.ts
+++ client/src/components/Editor/configs/menuConfig.ts
@@ -1,23 +1,4 @@
 import { menu, menuPlugin } from "@milkdown/plugin-menu";
-// import {
-//   EditorState,
-//   MarkType,
-//   liftListItem,
-//   redo,
-//   setBlockType,
-//   sinkListItem,
-//   TextSelection,
-//   wrapIn,
-// } from "@milkdown/prose";
-
-// const hasMark = (state: EditorState, type: MarkType): boolean => {
-//   if (!type) return false;
-//   const { from, $from, to, empty } = state.selection;
-//   if (empty) {
-//     return !!type.isInSet(state.storedMarks || $from.marks());
-//   }
-//   return state.doc.rangeHasMark(from, to, type);
-// };
 
 export default menu.configure(menuPlugin, {
   config: [
@@ -26,28 +7,38 @@ export default menu.configure(menuPlugin, {
         type: "button",
         icon: "bulletList",
         key: "WrapInBulletList",
-        // disabled: (view) => {
-        //   const { state } = view;
-        //   return !wrapIn(state.schema.nodes.bullet_list)(state);
-        // },
       },
       {
         type: "button",
         icon: "orderedList",
         key: "WrapInOrderedList",
-        // disabled: (view) => {
-        //   const { state } = view;
-        //   return !wrapIn(state.schema.nodes.ordered_list)(state);
-        // },
       },
       {
         type: "button",
         icon: "taskList",
         key: "TurnIntoTaskList",
-        // disabled: (view) => {
-        //   const { state } = view;
-        //   return !wrapIn(state.schema.nodes.task_list_item)(state);
-        // },
+      },
+    ],
+    [
+      {
+        type: "button",
+        icon: "image",
+        key: "InsertImage",
+      },
+      {
+        type: "button",
+        icon: "table",
+        key: "InsertTable",
+      },
+      {
+        type: "button",
+        icon: "code",
+        key: "TurnIntoCodeFence",
+      },
+      {
+        type: "button",
+        icon: "quote",
+        key: "WrapInBlockquote",
       },
     ],
   ],
