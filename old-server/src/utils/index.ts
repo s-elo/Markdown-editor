2441645ff911bf1487f5ed0dfdf3c57a9a8f5af5
s-elo <zz321yy@qq.com>
2025-08-12 18:38:13 +0800

    chore: remove old server

--- old-server/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-
-export const projectRoot = (...args: string[]) => path.resolve(__dirname, '../../../', ...args);
-
-export const normalizePath = (pathArr: string[]) => encodeURIComponent(pathArr.join('/'));
-
-export const denormalizePath = (pathStr: string) => decodeURIComponent(pathStr).split('/');
